432206b3670dd0583b228433b7218bd13401bfd7
kxusx <khush99199@gmail.com>
2022-03-08 20:26:44 +0530

    1

--- townHall.py
+++ townHall.py
@@ -5,21 +5,21 @@ from building import Building
 
 
 class TownHall(Building):
-    def __init__(self,game,x,y):
-        super().__init__(game,x,y,5,5,'T')
+    def __init__(self,game,y,x):
+        super().__init__(game,y,x,5,5,'T')
 
     def display(self):
         if(self.health>0):
-            y = self.y
             x = self.x
+            y = self.y
             w = self.width
             h = self.height
             arr = self.game.board
             cArr = self.game.cboard
-            for j in range(self.y-int(h/2)-1,self.y+int(h/2)):
-                for i in range(self.x-int(w/2)-1, self.x+int(w/2)):     
+            for j in range(self.x-int(h/2)-1,self.x+int(h/2)):
+                for i in range(self.y-int(w/2)-1, self.y+int(w/2)):     
                     arr[j][i] = self.color + self.char
-                    arr[j][self.x + int(w/2)] = Style.RESET_ALL + arr[j][self.x + w]
+                    arr[j][self.y + int(w/2)] = Style.RESET_ALL + arr[j][self.y + w]
                     cArr[j][i] = self.char
             self.game.board = arr
             self.game.cboard = cArr
